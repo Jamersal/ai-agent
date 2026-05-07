f93ceb137a3b805e44b2ad8a8a3031b37012ef4d
James Amyouny <jamesamyouny@gmail.com>
2026-05-07 11:38:59 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+from google.genai import types
+
+
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("some helpful message") 
+client = genai.Client(api_key=api_key)
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents=messages
+)
+
+
+if response.usage_metadata == None:
+    raise RuntimeError("some helpful message") 
+    
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+print(response.text)
