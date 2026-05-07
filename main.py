813a21e74d073670fe6580974784965036257ea5
James Amyouny <jamesamyouny@gmail.com>
2026-05-07 11:44:33 -0400

    added mutiple mssages

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
-import argparse
 from google.genai import types
 
 
+def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-# Now we can access `args.user_prompt`
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
-    raise RuntimeError("some helpful message") 
-client = genai.Client(api_key=api_key)
-
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=messages
-)
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages)
 
 
-if response.usage_metadata == None:
-    raise RuntimeError("some helpful message") 
-    
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
-print(response.text)
+if __name__ == "__main__":
+    main()
